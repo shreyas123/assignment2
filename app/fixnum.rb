c71eb2cd5d63263ce3a313d042f9c9af34b39d34
shreyasagarwal <shreyas.agarwal@artirix.com>
2013-10-02 13:30:35 +0530

    Moved names to CONSTANT

--- app/fixnum.rb
+++ app/fixnum.rb
@@ -1,8 +1,11 @@
 class Fixnum
+	THREEVALUE = 'Fizz'
+	FIVEVALUE = 'Buzz'
+
 	def output
-		return 'FizzBuzz' if(self % 15 == 0)
-		return 'Fizz' if(self % 3 == 0)
-		return 'Buzz' if(self % 5 == 0)
+		return THREEVALUE + FIVEVALUE if(self % 15 == 0)
+		return THREEVALUE if(self % 3 == 0)
+		return FIVEVALUE if(self % 5 == 0)
 		return self
 	end
 end
