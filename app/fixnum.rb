f3b4336a20ed3976f9d4b39306f336e19e986bb5
shreyasagarwal <shreyas.agarwal@artirix.com>
2013-10-02 10:55:38 +0530

    returns buzz when multiple of 5

--- app/fixnum.rb
+++ app/fixnum.rb
@@ -1,5 +1,7 @@
 class Fixnum
 	def output
 		return 'Fizz' if(self % 3 == 0)
+		return 'Buzz' if(self % 5 == 0)
+		return self
 	end
 end
--- spec/fixnum_spec.rb
+++ spec/fixnum_spec.rb
@@ -3,12 +3,21 @@ require 'spec_helper'
 describe Fixnum do
 	describe '#output' do 
 		it "should return 'Fizz' instead of multiple of 3" do 
-			expect(3.output).to eq   	'Fizz'
-			expect(6.output).to eq   	'Fizz'
-			expect(300.output).to eq 	'Fizz'
-			expect(1.output).not_to eq  'Fizz'
-			expect(2.output).not_to eq  'Fizz'
+			expect(3.output).to eq   	  'Fizz'
+			expect(6.output).to eq   	  'Fizz'
+			expect(300.output).to eq 	  'Fizz'
+			expect(1.output).to eq    1
+			expect(2.output).to eq    2
 			expect(302.output).not_to eq  'Fizz'
 		end
+
+		it "should return 'Buzz' instead of multiple of 3" do 
+			expect(5.output).to eq   	  'Buzz'
+			expect(10.output).to eq   	  'Buzz'
+			expect(100.output).to eq 	  'Buzz'
+			expect(1.output).not_to eq    'Buzz'
+			expect(2.output).not_to eq    'Buzz'
+			expect(302.output).not_to eq  'Buzz'
+		end		
 	end
 end
